85184f183519910a22062acd91ee8127bd48ebdb
Emerson MX <emersonmx@gmail.com>
2018-09-08 05:26:36 -0300

    Update macros

--- src/mng/macros.h
+++ src/mng/macros.h
@@ -3,18 +3,13 @@
 
 #include <assert.h>
 
-#define RETURN_IF_NULL(X) if (X == NULL) { \
-        return; \
-    }
-
 #define RETURN_R_IF_TRUE(X, R) if (X) { \
         return R; \
     }
 
 #define RETURN_R_IF_NULL(X, R) RETURN_R_IF_TRUE(X == NULL, R)
-
+#define RETURN_IF_NULL(X) RETURN_R_IF_NULL(X,)
 #define RETURN_NULL_IF_NULL(X) RETURN_R_IF_NULL(X, NULL)
-
 #define ASSERT_VALID_OBJECT(X) assert(X != NULL)
 
 #endif /* MNG_MACROS_H */
